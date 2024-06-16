d3ed6f41789f900bd4ea776f26eb53fc6ba81ce3
Eshan Chathuranga <171761693+eshanchathuranga@users.noreply.github.com>
2024-06-17 02:09:14 +0530

    Add files via upload

---
+++ Authentication/Using_Email_And_Password/LogIn/index.js
@@ -0,0 +1,35 @@
+// import firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+const firebaseConfig = {/*Config Here!*/};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+
+// Get element in html page
+let emailInput = document.getElementById('email-input');
+let passwordInput = document.getElementById('password-input');
+let signUpButton = document.getElementById('submit-button');
+
+signUpButton.addEventListener('click', function(){
+     // Sign up with email and password.
+     signInWithEmailAndPassword(auth, emailInput.value, passwordInput.value).then((userCredential) => {
+        //Logged In
+        const user = userCredential;
+        console.log(user)
+        alert('Login Successfully......!')
+        
+     }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // alert error
+        alert(errorCode + ': {' + errorMessage + '}')
+      });
+})
